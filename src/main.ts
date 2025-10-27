b86314e9e18cb62d6694b9435b58eaac6b34b8b6
FilipeHaniel <filipe.haniel@gmail.com>
2025-10-27 17:58:06 -0300

    insatll pg

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,30 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
+import { Client } from 'pg'
 
 async function bootstrap() {
   const port = process.env.PORT || 3000
   const app = await NestFactory.create(AppModule, { cors: true })
+
+  async function testSupabaseConnection() {
+    const client = new Client({
+      connectionString: process.env.DATABASE_URL,
+    });
+  
+    try {
+      console.log('🧪 Testando conexão com o Supabase...');
+      await client.connect();
+      console.log('✅ Conexão com Supabase bem-sucedida!');
+      await client.end();
+    } catch (error) {
+      console.error('❌ Falha ao conectar ao Supabase:');
+      console.error(error);
+    }
+  }
+  
+  testSupabaseConnection();
+
+
   await app.listen(port)
   console.log(`🚀 Server is running on http://localhost:${port} port`)
 }
